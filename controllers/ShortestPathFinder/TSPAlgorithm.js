65bf500b0a87c36f24fcac5ba7e3986851a0d152
Aishwarya Kaneri <akaneri@paypal.com>
2016-04-26 20:55:00 +0530

    develop TSP dynamic prog

--- controllers/ShortestPathFinder/TSPAlgorithm.js
+++ controllers/ShortestPathFinder/TSPAlgorithm.js
@@ -4,11 +4,58 @@
  Built for the pattern of an async.waterfall.
  Callback first param is for errors.
  */
-var tspDynamicAlgo = function tspDynamicAlgo(locations, positions, graph, callback) {
-    var shortestPathNodes = [];
+var shortestPathNodes = [];
 
+var tspDynamicAlgo = function tspDynamicAlgo(locations, positions, graph, callback) {
+    var start = 0;
+    var sample = [];
+    for (var i = 1; i < graph.length; i++) {
+        sample.push(i);
+    }
+    var traversal = [0];
+    var data = getShortestPath(graph, start, sample, traversal);
+    data.traversal.push(0);
+    shortestPathNodes = data.traversal;
     callback(null, locations, positions, shortestPathNodes);
 };
+
+var getShortestPath = function getShortestPath(graph, start, sample) {
+    if (sample.length === 1) {
+        var traversal = [0];
+        traversal.push(sample[0]);
+        var distance = graph[start][sample[0]] + graph[sample[0]][0];
+        var data = {
+            traversal: traversal,
+            distance: distance
+        };
+        return data;
+    }
+    var combinationCosts = [];
+    var traversals = [];
+    var n = sample.length;
+    for (var k = 0; k < n; k++) {
+        var s = [];
+        for (var i = 0; i < n; i++) {
+            if (i != k) {
+                s.push(sample[i]);
+            }
+        }
+        var data = getShortestPath(graph, sample[k], s);
+        data.traversal.push(sample[k]);
+        traversals.push(data.traversal);
+        combinationCosts.push(graph[start][sample[k]] + data.distance);
+    }
+    var min = getMin(combinationCosts);
+    var minTraversal = traversals[combinationCosts.indexOf(min)];
+    return {
+        traversal: minTraversal,
+        distance: min
+    };
+};
+
+var getMin = function getMin(costs) {
+    return Math.min.apply(Math, costs);
+}
 
 module.exports = {
     tspDynamicAlgo: tspDynamicAlgo
--- test/TSPAlgorithm.test.js
+++ test/TSPAlgorithm.test.js
@@ -8,11 +8,15 @@ chai.config.includeStack = true;
 
 describe('TSPAlgorithm.test', function () {
     describe('tspDynamicAlgo', function () {
-        it('Should return shortest path (nodes) ', function (done) {
+        //it('Should return same node if there is only one node', function(done) {});
+        //it('Should return pair if there are only two nodes', function(done) {});
+        //it('Should return in order if all three nodes are in a line', function(done) {});
+        //it('Should return in travel order if three nodes are in a line, but given out of order', function(done){});
+        it('Should return shortest path for any arbitrary number of nodes ', function (done) {
             var locations = [];
             var positions = [];
             var graph = [[0, 7, 5, 10], [7, 0, 6, 4], [5, 6, 0, 8], [10, 4, 8, 0]];
-            var expectedOutput = [0, 2, 1, 3, 0];
+            var expectedOutput = [0, 2, 3, 1, 0];
             tspAlgorithm.tspDynamicAlgo(locations, positions, graph, function (error, locations, positions, shortestPathNodes) {
                 console.log("tspDynamicAlgo - actual = " + shortestPathNodes + ", expected = " + expectedOutput);
                 expect(shortestPathNodes).to.eql(expectedOutput);
